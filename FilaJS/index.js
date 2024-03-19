0cb96132f39fcf25d85e6a9c01a1737d22e1b7c3
mock <124210151+mockjk@users.noreply.github.com>
2024-03-19 12:44:43 -0300

    Add files via upload

---
+++ FilaJS/index.js
@@ -0,0 +1,7 @@
+import { addItemQueue, createQueue, removeItemQueue } from "./function.js";
+
+let novaFila = createQueue(3)
+
+removeItemQueue2(novaFila)
+console.log("-------------------------")
+console.log(novaFila)
